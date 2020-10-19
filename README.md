1526ae4423d5ba4ad5953d789e28be169a9ccdff
eyale <eyale@outlook.com>
2020-10-19 18:50:18 +0300

    a few more changes

--- README.md
+++ README.md
@@ -2,8 +2,9 @@
 
 ## Zone.runOutsizeAngular() demo
 You can find this demo under `/zone`. The components associated with this view are `/views/zone` and `/components/array-sorter`.
-* Change the add items to the array in `zone.component.ts` and the browser will choke
+* Add items to the array in `zone.component.ts` (8 items should be enough) and the browser will choke
 * Change `array-sorter.component.ts` to `OnPush` and watch the problem go away
+* You can also comment in/out `this.zone.runOutsideAngular(...)` to see the difference
 
 ## Basic OnPush demo
 You can find this demo under `/basic`. The components associated with this view are `/views/basic` and `components/user-component`. 
@@ -16,7 +17,7 @@ You can find this demo under `/advanced`. The components associated with this vi
 
 * Run the demo and change a 2nd level component to OnPush. Angular will stop detecting changes every 2 seconds
 * In `card.component.html` uncomment the observable binding with the async pipe. Change detection will work for OnPush components.
-* In `card.component.html` uncomment the binding to `rndValue` and it won't work
+* In `card.component.html` uncomment the binding to `rndValue`
     * It won't work
     * Use `cdr.markForCheck()` in the setter in `on-push.card.component.ts` and it'll detect changes again.
 
--- src/app/views/zone/zone.component.ts
+++ src/app/views/zone/zone.component.ts
@@ -1,45 +1,5 @@
 import { Component, OnInit, NgZone } from '@angular/core';
 const initialArray = [1, 3, 2, 4, 5, 6, 7, 8];
-
-function Bogosort(arr: number[]) {
-  function isSorted(targetArray: number[]) {
-    for (let i = 1; i < targetArray.length; i++) {
-      if (targetArray[i - 1] > targetArray[i]) {
-        return false;
-      }
-    }
-    return true;
-  }
-
-  function shuffle(targetArray: number[]) {
-    let count = targetArray.length;
-    let temp: number;
-    let index: number;
-
-    while (count > 0) {
-      index = Math.floor(Math.random() * count);
-      count--;
-
-      temp = targetArray[count];
-      targetArray[count] = targetArray[index];
-      targetArray[index] = temp;
-    }
-
-    return targetArray;
-  }
-
-  function sort(targetArray: number[]) {
-    let sorted = false;
-    while (!sorted) {
-      targetArray = shuffle(targetArray);
-      sorted = isSorted(targetArray);
-    }
-    return targetArray;
-  }
-
-  return sort(arr);
-}
-
 @Component({
   selector: 'app-zone',
   templateUrl: './zone.component.html',
@@ -50,20 +10,11 @@ export class ZoneComponent implements OnInit {
   constructor(private zone: NgZone) {}
 
   ngOnInit() {
-    // -------------- INSTRUCTIONS --------------------
-    // Bogossort() is a very non-performant sorting algorithm.
-    // To make it hurt the CPU add elements to `initialArray`.
-    // On an i7 machine I could feel the browser suffering with 8 elements.
-    // The components on the screen will light up every time a change detection is occuring.
-
-    // To reduce the amount of calls to this function we can remove `mousemove` from
-    // the Angular Zone by using `ngZone.runOuutsideAngular()`.
-
-    // ----------- comment in and out to see difference -------------
-    // this.zone.runOutsideAngular(() => {
-    window.document.addEventListener('mousemove', (ev: MouseEvent) => {
-      Bogosort(initialArray);
+    // comment `this.zone.runOutsideAngular()` in and out to see the difference
+    this.zone.runOutsideAngular(() => {
+      window.document.addEventListener('mousemove', (ev: MouseEvent) => {
+        // now the event is registered with our zone
+      });
     });
-    // });
   }
 }
